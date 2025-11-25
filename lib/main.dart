1b5b85d0c8742bca4aadf0697739d5efe2353ea5
Abhishek <abhishek@Abhisheks-MacBook-Air.local>
2025-11-25 18:51:46 +0530

    Firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:booknest/firebase_options.dart';
 import 'package:booknest/screens/main_navigation.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const BookNestApp());
 }
 
